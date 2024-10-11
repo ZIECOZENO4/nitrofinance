0499bf66c0e3b3382d59aafba915e4c30a120752
ZIECOZENO4 <ziecozeno4@gmail.com>
2024-10-11 21:51:18 +0100

    ghfkygk

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    swcMinify: false, // Disable SWC minification
-    webpack: (config, { isServer }) => {
-      // Use import instead of require
-      config.resolve.alias['@babel/runtime'] = import.meta.url.replace(/\/[^/]+$/, '') + '/node_modules/@babel/runtime';
-      
-      // Add any other custom webpack configurations here
-      
-      return config;
-    },
-    // Optionally, you can add experimental features if needed
-    experimental: {
-      // Example: esmExternals: true,
-    },
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
